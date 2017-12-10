2328b0c90446579c0a4d0ae40227570addd98c32
luluq <luluq@beritagar.id>
2017-12-11 00:05:50 +0700

    update

--- app/Http/Controllers/ApiGetController.php
+++ app/Http/Controllers/ApiGetController.php
@@ -839,7 +839,7 @@ class ApiGetController extends BaseController
             }
 
         }
-        print_r($params);
+        print_r($params);die();
 
         $client = \Elasticsearch\ClientBuilder::create()           // Instantiate a new ClientBuilder
         ->setHosts($this->host)      // Set the hosts
